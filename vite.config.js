25214b3a14d4ea51e2d77b9fda98e429142b9ad0
rkdwlss2 <rkdwlss2@gmail.com>
2026-07-19 19:58:24 +0900

    리액트 추가

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),
+        board: resolve(import.meta.dirname, "board.html"),
+      },
+    },
+  },
+});
